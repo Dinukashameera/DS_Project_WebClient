8b8fb43b225544b9054b49985e10ce01d816fc9a
DInuwan97 <kalubowila.dc25132@live.com>
2020-04-10 12:52:02 +0530

    Dummy Sensor Added

---
+++ Dummy Sensor/DummySensor/DummySensor/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DummySensor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRoomNo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCO2 = new System.Windows.Forms.Label();
+            this.lblSmoke = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(312, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Room Number";
+            // 
+            // txtRoomNo
+            // 
+            this.txtRoomNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRoomNo.Location = new System.Drawing.Point(290, 81);
+            this.txtRoomNo.Name = "txtRoomNo";
+            this.txtRoomNo.Size = new System.Drawing.Size(220, 34);
+            this.txtRoomNo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(184, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CO2 Level";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(184, 279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Smoke Level";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(313, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 54);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start Sensor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCO2
+            // 
+            this.lblCO2.AutoSize = true;
+            this.lblCO2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCO2.Location = new System.Drawing.Point(431, 229);
+            this.lblCO2.Name = "lblCO2";
+            this.lblCO2.Size = new System.Drawing.Size(135, 29);
+            this.lblCO2.TabIndex = 5;
+            this.lblCO2.Text = "CO2 Level";
+            // 
+            // lblSmoke
+            // 
+            this.lblSmoke.AutoSize = true;
+            this.lblSmoke.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSmoke.Location = new System.Drawing.Point(431, 279);
+            this.lblSmoke.Name = "lblSmoke";
+            this.lblSmoke.Size = new System.Drawing.Size(135, 29);
+            this.lblSmoke.TabIndex = 6;
+            this.lblSmoke.Text = "CO2 Level";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSmoke);
+            this.Controls.Add(this.lblCO2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRoomNo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRoomNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCO2;
+        private System.Windows.Forms.Label lblSmoke;
+    }
+}
+
