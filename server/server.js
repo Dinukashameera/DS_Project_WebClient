db799c80bb5af31f335f42a54c1e7ab419f4811d
Dinuka Shameera <dinukashameera0657@gmail.com>
2020-04-23 11:31:14 +0530

    completed the mail sending part 100%

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use("/api/users", users);
 app.use("/api/room", rooms);
 //connecting to the dataBase
 mongoose
-  .connect("mongodb://localhost:27017/ds_proj_remote", {
+  .connect("mongodb+srv://ds123:ds123@dswebprojectcluster-jvrhf.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
